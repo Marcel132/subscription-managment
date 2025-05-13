ebefeb6e9ba9afcfe25883ed91f3a0a70f7f0eb8
Marcel <marcelpodsiadlo.business@gmail.com>
2025-05-13 12:32:52 +0200

    create defualt layout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-		RouterOutlet
+		CommonModule,
+		RouterOutlet,
 	],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
-	isUserLogged: boolean = false
+	isUserLogged: boolean = true
 	constructor(){}
 }
