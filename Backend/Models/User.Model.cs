9f672d2a219cddc3eca23b905c38c3e1a3dcf585
Marcel <marcelpodsiadlo.business@gmail.com>
2025-05-14 20:01:43 +0200

    Add PostgreSQL

---
+++ Backend/Models/User.Model.cs
@@ -0,0 +1,10 @@
+namespace SubscriptionApi.Modules;
+
+public class User 
+{
+  public int Id { get; set; }
+  public string Email { get; set; } = string.Empty;
+  public string Password { get; set; } = string.Empty;
+  public string Role { get; set; } = "Free";
+  public int UserID { get; set; }
+}
